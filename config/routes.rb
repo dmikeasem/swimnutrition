7e12d38b7ca650ab92e8eaff261dba95b9d5f4ee
dmikeasem <d.mike.asem@gmail.com>
2014-03-21 00:19:42 -0400

    Added Products page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Pinteresting::Application.routes.draw do
   devise_for :users
   root "posts#index"
   get "about" => "pages#about"
-  get "contributors" => "pages#contributors"
+  get "products" => "pages#products"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
