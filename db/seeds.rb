eae08072b2ad0fbe2cb68f2f878ae563d4201de7
dmikeasem <d.mike.asem@gmail.com>
2014-03-21 03:13:59 -0400

    Resolved Admin issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(:name => "Rachel", :password => "iloverachel", :password_confirmation => "iloverachel", :email => "rachel.sitarz1@gmail.com", :admin => true)
