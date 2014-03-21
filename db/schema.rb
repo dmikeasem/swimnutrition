4ebc92be82308e090423d920499e67771a90eb10
dmikeasem <d.mike.asem@gmail.com>
2014-03-20 22:35:29 -0400

    Added name column to users, Added name to posts, name validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319205236) do
+ActiveRecord::Schema.define(version: 20140321020350) do
 
   create_table "posts", force: true do |t|
     t.string   "description"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140319205236) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
