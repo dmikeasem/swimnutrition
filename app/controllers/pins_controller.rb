ce60752e4301bb73fb820d827d04af87646f964f
dmikeasem <d.mike.asem@gmail.com>
2014-03-22 01:48:13 -0400

    Added pagination to pins#index

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 15)
   end
 
   def show
